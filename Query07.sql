42b40662cbd84dafafef5380c1e349e8c45c7d25
AKASH ANURAGI <136264753+AkashAnuragi@users.noreply.github.com>
2025-03-07 15:27:00 +0530

    Insert a single Data into table.

---
+++ Query07.sql
@@ -0,0 +1,8 @@
+# Insert a single Data into table.
+-- insert into students(stud_id,Stud_name, Stud_class, stud_city)values(02,'Akhil', 12, 'Delhi');
+
+# insert a multiple data into table 
+insert into students(stud_id,Stud_name, Stud_class, stud_city)
+values(03,'kartik', 12, 'Delhi'),
+(04,'Sagar', 11, 'Mumbai'),
+(05, 'Harsh',09, 'Noida');
